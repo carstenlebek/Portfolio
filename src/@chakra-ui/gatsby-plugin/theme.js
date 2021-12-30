10ff2dbf32740d4066c50882b1cc629504f01c50
konzeptfabrik <carsten.lebek@googlemail.com>
2021-12-30 18:44:26 +0100

    x

--- src/@chakra-ui/gatsby-plugin/theme.js
+++ src/@chakra-ui/gatsby-plugin/theme.js
@@ -1,8 +1,9 @@
 import { extendTheme } from "@chakra-ui/react"
 const theme = {
   fonts: {
-    heading: "Quicksand, sans-serif",
-    body: "Quicksand, sans-serif",
+    heading:
+      "Quicksand,'Helvetica Neue', 'Arial Nova', Helvetica, Arial, sans-serif",
+    body: "Quicksand,'Helvetica Neue', 'Arial Nova', Helvetica, Arial, sans-serif",
   },
   styles: {
     global: {
