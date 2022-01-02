c588e039c4f537d7a58c48f5c6c5c56d1c9c397d
konzeptfabrik <carsten.lebek@googlemail.com>
2022-01-02 12:35:25 +0100

    x

--- src/pages/projects/rscards.js
+++ src/pages/projects/rscards.js
@@ -26,8 +26,8 @@ export default function RsCards({ location, data }) {
     ],
   }
 
-  const initialImageTransitionPosition =
-    location.state.initialImageTransitionPosition
+  const initialImageTransitionPosition = location.state
+    ?.initialImageTransitionPosition || { bottom: 0, top: 0, left: 0, right: 0 }
 
   const image = getImage(
     data.allFile.nodes.find(
