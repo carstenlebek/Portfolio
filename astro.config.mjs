0cd4683083fd67f84feefc06b13c2edea96eb4a4
carstenlebek <carsten.lebek@googlemail.com>
2022-06-27 22:28:17 +0200

    :sparkles: initial astro commit

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [preact(), tailwind()]
+});
