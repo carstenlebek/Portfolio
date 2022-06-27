729234d74e9183fc03413fa5df53a77c818e96a9
carstenlebek <carsten.lebek@googlemail.com>
2022-06-27 22:36:48 +0200

    cleanup

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [preact(), tailwind()]
-});
